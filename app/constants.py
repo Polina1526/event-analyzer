c74f36bf766579c04a336c7a250c2230fc7605a6
polina_cherepanova <polina2603cherri@gmail.com>
2025-05-08 23:03:57 +0300

    some changes

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+import typing
+
+
+TIMESTAMP_COL_NAME: typing.Final[str] = "timestamp"
+TREADID_COL_NAME: typing.Final[str] = "threadid"
+EVENT_TYPE_COL_NAME: typing.Final[str] = "event_type"
+
+DEFAULT_WINDOW_DAYS: typing.Final[int] = 10
