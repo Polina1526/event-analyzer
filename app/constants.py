ff409567832e64dde18b5a0b55323d9ab4c8c2e7
polina_cherepanova <polina2603cherri@gmail.com>
2025-05-09 14:03:05 +0300

    some package update

--- app/constants.py
+++ app/constants.py
@@ -8,3 +8,4 @@ EVENT_TYPE_COL_NAME: typing.Final[str] = "event_type"
 EVENT_CHAIN_ID_CON_NAME: typing.Final[str] = "event_chain_id"
 
 DEFAULT_WINDOW_DAYS: typing.Final[int] = 10
+DEFAULT_TRAIN_DATA_COUNT: typing.Final[int] = 50_000
